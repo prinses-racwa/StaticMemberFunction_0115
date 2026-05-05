521d70998fec3ab6c020ea993dd8ab6bfcda3a07
prinses-racwa <rajwaamelia@gmail.com>
2026-05-05 19:43:42 +0700

    Membuat class produk dengan berbagai constructor overloading

--- destrucktor.cpp
+++ destrucktor.cpp
@@ -1,3 +1,37 @@
 #include <string>
 #include <iostream>
 using namespace std;
+
+class produk {
+private:
+    int id;
+    string nama;
+
+public:
+
+    void tampilkan() {
+        cout << "id: " << id << endl;
+        cout << "Nama: " << nama << endl;
+        cout << endl;
+    }
+
+    produk() {
+        id = 14;
+        nama = "tali";
+    }
+
+    produk (int pid){
+        id=pid;
+        nama="meja";
+    }
+
+    produk (string pnama){
+        id = 1;
+        nama=pnama;
+    }
+
+    produk (int pid, string pnama) {
+        id=pid;
+        nama=pnama;
+    }
+};
