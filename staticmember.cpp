a3eae54a91e62871935e8de1c7d4f7332ff2412c
prinses-racwa <rajwaamelia@gmail.com>
2026-05-05 18:38:43 +0700

    menambahkan printAll

--- staticmember.cpp
+++ staticmember.cpp
@@ -20,3 +20,10 @@ int mahasiswa::nim = 10;
 void mahasiswa::setID(){
     id = ++nim;
 };
+
+void mahasiswa::printAll(){
+
+    cout << " ID : " <<id << endl;
+    cout << "nama : " <<nama << endl;
+    cout << endl;
+};
